773ced7466c90712f8150dc8375b35da6105f188
Ap3x99 <74175693+Ap3x99@users.noreply.github.com>
2021-09-02 14:17:04 -0500

    Added camera locations
    Added CCTV to most common robbery locations in roleplay servers.

--- config.lua
+++ config.lua
@@ -7,19 +7,143 @@ SecurityCamConfig.Locations = {
     {
         camBox = {label = "Pacific Standard Bank", x = 283.67, y = 264.41, z = 105.80},
         cameras = {
-            {label = "Camera 1", x = 257.45, y = 210.07, z = 109.08, r = {x = -25.0, y = 0.0, z = 28.05}, canRotate = false},
-            {label = "Camera 2", x = 269.66, y = 223.67, z = 113.23, r = {x = -30.0, y = 0.0, z = 111.29}, canRotate = false},
-            {label = "Camera 3", x = 241.64, y = 233.83, z = 111.48, r = {x = -35.0, y = 0.0, z = 120.46}, canRotate = false},
-            {label = "Camera 4", x = 232.86, y = 221.46, z = 107.83, r = {x = -25.0, y = 0.0, z = -140.91}, canRotate = false},
-            {label = "Camera 5", x = 261.50, y = 218.08, z = 107.95, r = {x = -25.0, y = 0.0, z = -149.49}, canRotate = false},
-            {label = "Camera 6", x = 261.98, y = 217.92, z = 113.25, r = {x = -40.0, y = 0.0, z = -159.49}, canRotate = false},
-            {label = "Camera 7", x = 258.44, y = 204.97, z = 113.25, r = {x = -30.0, y = 0.0, z = 10.50}, canRotate = false},
-            {label = "Camera 8", x = 235.53, y = 227.37, z = 113.23, r = {x = -35.0, y = 0.0, z = -160.29}, canRotate = false},
-            {label = "Camera 9", x = 254.72, y = 206.06, z = 113.28, r = {x = -35.0, y = 0.0, z = 44.70}, canRotate = false},
-            {label = "Camera 10", x = 269.89, y = 223.76, z = 106.48, r = {x = -35.0, y = 0.0, z = 112.62}, canRotate = false},
-            {label = "Camera 11", x = 252.27, y = 225.52, z = 103.99, r = {x = -35.0, y = 0.0, z = -74.87}, canRotate = false},
+            {label = "Camera 1", x = 257.45, y = 210.07, z = 109.08, r = {x = -25.0, y = 0.0, z = 28.05}, canRotate = true},
+            {label = "Camera 2", x = 269.66, y = 223.67, z = 113.23, r = {x = -30.0, y = 0.0, z = 111.29}, canRotate = true},
+            {label = "Camera 3", x = 241.64, y = 233.83, z = 111.48, r = {x = -35.0, y = 0.0, z = 120.46}, canRotate = true},
+            {label = "Camera 4", x = 232.86, y = 221.46, z = 107.83, r = {x = -25.0, y = 0.0, z = -140.91}, canRotate = true},
+            {label = "Camera 5", x = 261.50, y = 218.08, z = 107.95, r = {x = -25.0, y = 0.0, z = -149.49}, canRotate = true},
+            {label = "Camera 6", x = 261.98, y = 217.92, z = 113.25, r = {x = -40.0, y = 0.0, z = -159.49}, canRotate = true},
+            {label = "Camera 7", x = 258.44, y = 204.97, z = 113.25, r = {x = -30.0, y = 0.0, z = 10.50}, canRotate = true},
+            {label = "Camera 8", x = 235.53, y = 227.37, z = 113.23, r = {x = -35.0, y = 0.0, z = -160.29}, canRotate = true},
+            {label = "Camera 9", x = 254.72, y = 206.06, z = 113.28, r = {x = -35.0, y = 0.0, z = 44.70}, canRotate = true},
+            {label = "Camera 10", x = 269.89, y = 223.76, z = 106.48, r = {x = -35.0, y = 0.0, z = 112.62}, canRotate = true},
+            {label = "Camera 11", x = 252.27, y = 225.52, z = 103.99, r = {x = -35.0, y = 0.0, z = -74.87}, canRotate = true},
             {label = "Camera 12", x = 251.65, y = 218.66, z = 104.5, r = {x = 0.0, y = 0.0, z = -20.0}, canRotate = true}
         },
         allowedModels = {}
-    }
+    },
+    {
+        camBox = {label = "LTD Grove St.", x = -40.95, y = -1747.85, z = 29.33},
+        cameras = {
+            {label = "Camera 1", x = -56.84, y = -1752.08, z = 31.13, r = {x = -25.0, y = 0.0, z = -80.0}, canRotate = true},
+            {label = "Camera 2", x = -43.01, y = -1755.21, z = 31.56, r = {x = -25.0, y = 0.0, z =  95.0}, canRotate = true},
+            {label = "Camera 3", x = -43.48, y = -1748.51, z = 31.13, r = {x = -25.0, y = 0.0, z = -180.0}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Union Depository Primary", x = -42.59, y = -664.49, z = 33.66},
+        cameras = {
+            {label = "Camera 1", x = -1.25, y = -701.58, z = 18.14, r = {x = -25.0, y = 0.0, z = 19.51}, canRotate = true},
+            {label = "Camera 2", x = -6.90, y = -685.86, z = 18.14, r = {x = -25.0, y = 0.0, z =  196.51}, canRotate = true},
+            {label = "Camera 3", x = -0.86, y = -685.52, z = 18.39, r = {x = -25.0, y = 0.0, z = 7.51}, canRotate = true},
+            {label = "Camera 4", x = -5.29, y = -667.01, z = 18.02, r = {x = -25.0, y = 0.0, z = 294.63}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Union Depository Secondary", x = 4.49, y = -648.44, z = 31.89},
+        cameras = {
+            {label = "Camera 1", x = -1.25, y = -701.58, z = 18.14, r = {x = -25.0, y = 0.0, z = 19.51}, canRotate = true},
+            {label = "Camera 2", x = -6.90, y = -685.86, z = 18.14, r = {x = -25.0, y = 0.0, z =  196.51}, canRotate = true},
+            {label = "Camera 3", x = -0.86, y = -685.52, z = 18.39, r = {x = -25.0, y = 0.0, z = 7.51}, canRotate = true},
+            {label = "Camera 4", x = -5.29, y = -667.01, z = 18.02, r = {x = -25.0, y = 0.0, z = 294.63}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Fleeca Bank Legion Square", x = 146.15, y = -1058.66, z = 30.18},
+        cameras = {
+            {label = "Camera 1", x = 146.57, y = -1038.28, z = 30.81, r = {x = -25.0, y = 0.0, z = 239.43}, canRotate = true},
+            {label = "Camera 2", x = 145.03, y = -1042.94, z = 31.00, r = {x = -25.0, y = 0.0, z = 210.58}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Fleeca Bank Chumash", x = -2954.08, y = 473.80, z = 15.69},
+        cameras = {
+            {label = "Camera 1", x = -2963.91, y = 478.98, z = 17.33, r = {x = -25.0, y = 0.0, z = 31.27}, canRotate = true},
+            {label = "Camera 2", x = -2958.91, y = 478.97, z = 17.27, r = {x = -25.0, y = 0.0, z = 322.98}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "24/7 Sandy Shores", x = 1953.14, y = 3753.02, z = 32.21},
+        cameras = {
+            {label = "Camera 1", x = 1969.42, y = 3743.76, z = 34.54, r = {x = -25.0, y = 0.0, z = 86.01}, canRotate = true},
+            {label = "Camera 2", x = 1962.95, y = 3748.59, z = 34.54, r = {x = -25.0, y = 0.0, z = 68.00}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Paleto Savings Bank", x = -116.51, y = 6479.62, z = 31.46},
+        cameras = {
+            {label = "Camera 1", x = -113.09, y = 6464.45, z = 33.20, r = {x = -25.0, y = 0.0, z = 42.97}, canRotate = true},
+            {label = "Camera 2", x = -107.91, y = 6468.58, z = 34.20, r = {x = -25.0, y = 0.0, z = 225.88}, canRotate = true},
+            {label = "Camera 3", x = -104.59, y = 6479.44, z = 33.57, r = {x = -25.0, y = 0.0, z = 177.00}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Ammunation Rte. 68", x = -1127.60, y = 2708.02, z = 18.80},
+        cameras = {
+            {label = "Camera 1", x = -1122.44, y = 2697.90, z = 20.82, r = {x = -25.0, y = 0.0, z = 271.57}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Vangelico's Jewelry Store", x = -590.91, y = -282.60, z = 50.32},
+        cameras = {
+            {label = "Camera 1", x = -620.31, y = -224.48, z = 40.51, r = {x = -25.0, y = 0.0, z = 167.90}, canRotate = true},
+            {label = "Camera 2", x = -627.43, y = -239.76, z = 40.51, r = {x = -25.0, y = 0.0, z = 335.90}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "24/7 Rte. 15", x = 2546.42, y = 385.58, z = 108.61},
+        cameras = {
+            {label = "Camera 1", x = 2558.99, y = 390.64, z = 110.82, r = {x = -25.0, y = 0.0, z = 133.31}, canRotate = true},
+            {label = "Camera 2", x = 2551.47, y = 387.88, z = 110.95, r = {x = -25.0, y = 0.0, z = 134.71}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Ammunation Rte. 15", x = 2558.81, y = 288.64, z = 108.60},
+        cameras = {
+            {label = "Camera 1", x = 2571.75, y = 291.58, z = 110.87, r = {x = -25.0, y = 0.0, z = 38.62}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "24/7 Zancudo", x = -2521.18, y = 2310.52, z = 33.21},
+        cameras = {
+            {label = "Camera 1", x = -2549.29, y = 2314.61, z = 35.70, r = {x = -25.0, y = 0.0, z = 228.45}, canRotate = true},
+            {label = "Camera 2", x = -2545.68, y = 2307.44, z = 35.70, r = {x = -25.0, y = 0.0, z = 214.12}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Fleeca Rte. 68", x = 1194.24, y = 2721.66, z = 38.81},
+        cameras = {
+            {label = "Camera 1", x = 1179.08, y = 2705.57, z = 39.72, r = {x = -25.0, y = 0.0, z = 88.81}, canRotate = true},
+            {label = "Camera 2", x = 1178.87, y = 2710.60, z = 39.72, r = {x = -25.0, y = 0.0, z = 62.07}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "Ammunation Sandy Shores", x = 1687.71, y = 3755.81, z = 34.71},
+        cameras = {
+            {label = "Camera 1", x = 1689.17, y = 3758.89, z = 37.03, r = {x = -25.0, y = 0.0, z = 278.79}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "24/7 Procopio Beach", x = 1723.37, y = 6417.72, z = 35.00},
+        cameras = {
+            {label = "Camera 1", x = 1736.07, y = 6409.51, z = 37.11, r = {x = -25.0, y = 0.0, z = 15.42}, canRotate = true},
+            {label = "Camera 1", x = 1736.61, y = 6417.55, z = 37.24, r = {x = -25.0, y = 0.0, z = 25.68}, canRotate = true},
+        },
+        allowedModels = {}
+    },
+}
 }
